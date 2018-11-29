f156b0d73ab2264e8b81b1e89fa00fc84cf1984f
Argenis <bernyargenis@yaho.com>
2018-11-29 02:02:07 -0600

    Login normal 2

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken')
+//===================
+// Verificar token
+//===================
+
+let verificaToken = (req, res, next)=>{
+    let token = req.get('token');
+    jwt.verify(token, process.env.SEED, (err, decoded)=>{
+        if(err){
+            return res.status(401).json({
+                ok:false,
+                err:{
+                    message: 'Token no valido'
+                }
+            })
+        }
+        req.usuario = decoded.usuario;
+        next();
+    })
+};
+
+//===================
+// Verificar ADMIN_ROLE
+//===================
+
+let verificaAdminRole = (req, res, next)=>{
+    let usuario = req.usuario;
+    if (usuario.role !== 'ADMIN_ROLE') {
+        return res.json({
+            ok:false,
+            err:{
+                message: 'No posee permisos de ADMIN'
+            }
+        })
+    }
+  
+    next();
+};
+
+module.exports = {
+    verificaToken,
+    verificaAdminRole
+}
